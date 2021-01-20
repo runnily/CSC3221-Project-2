d56fde39693f60eaf5d3987b28d306e94a5857b2
runnily <32201807+runnily@users.noreply.github.com>
2021-01-20 20:26:36 +0000

    changed test

--- submission/game.cpp
+++ submission/game.cpp
@@ -219,7 +219,9 @@ vector<Shape*> setup(int numberOfSquares, int numberOfCircle, coordinate dim) {
 
 int main() {
     coordinate dim = {500,500}; // <-- adjust size here
-    vector<Shape*> v = setup(50, 60, dim); // <-- adjust number here
+    int numberOfSqaure = 50;
+    int numberOfCircle = 60;
+    vector<Shape*> v = setup(numberOfSqaure , numberOfCircle, dim); 
     Game start = Game(dim, v);
     start.play();
    
